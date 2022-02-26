1bc3d90d2fac3acb1b1f9fb71144cdd47fd29801
Akposieyefa <orutu1@gmail.com>
2022-02-26 12:35:53 +0100

    code clean up

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.browserSync('http://127.0.0.1:8000/');
+mix.browserSync('https://payroll-laravel-vue.herokuapp.com/');
 
 mix.js('resources/js/app.js', 'public/js').vue()
     .postCss('resources/css/app.css', 'public/css', [
